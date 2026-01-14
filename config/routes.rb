2a596ae19f466d6829bc67cec81964fad5bf6516
高橋結翔 <jiexianggaoqiao71@gmail.com>
2026-01-14 13:50:17 +0900

    Add user signup with sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # ユーザー登録
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+
   root "tops#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
