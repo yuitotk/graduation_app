bcb92176ded7e7d9f199a67db985efba3c665d3a
高橋結翔 <jiexianggaoqiao71@gmail.com>
2026-03-25 19:40:27 +0900

    Adjust auth and inquiry form layouts

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -4,6 +4,6 @@ class Inquiry < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
 
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 150 }, format: { with: VALID_EMAIL_REGEX }
   validates :body,  presence: true, length: { maximum: 2000 }
 end
